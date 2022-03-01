43a5fb1204798b5da57db912c36dd56c48221b7a
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2022-03-01 09:28:19 +0100

    Add project files.

---
+++ OOP_Clovek/Clovek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Clovek
+{
+    public class Clovek
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        public int Energie { get; set; }
+        public int Zivot { get; set; }
+        public int Dovednost { get; set; }
+
+        public Clovek()
+        {
+            Jmeno = "";
+            Prijmeni = "";
+            DatumNarozeni = DateTime.Now;
+            Energie = 100;
+            Zivot = 100;
+            Dovednost = 0;
+        }
+
+    }
+}
